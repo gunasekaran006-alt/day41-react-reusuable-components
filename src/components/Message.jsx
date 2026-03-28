14b34444aaaa9ce45d48aa3094200f9a3c2d869f
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-03-28 22:44:39 +0530

    updates

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Button from './Button'
 
+
 function Message() {
-const title = "Click to read this messsage"
+const title = "Click to read this message"
+
+function test (){
+  console.log("Test");
+}
 
   return (
-    <div>
 
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat maxime iusto repellat labore, consectetur rerum magni mollitia aliquam accusamus, nesciunt voluptate, sint vel praesentium numquam facere eius veritatis nihil eum.</p>
-<Button name= {title} />
-    </div>
+<div>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, et pariatur sapiente rem tempora dicta illo necessitatibus! Consequatur accusantium nulla dolorem, accusamus voluptatum iure ipsam inventore magni quidem eum et.</p>
+<Button name = {title} logic ={test}/>
+</div>
+
   )
 }
 
